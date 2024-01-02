4e29a2b26c068f62cbbbfadc890cc6a6990f15f0
Will Sims <willsims14@gmail.com>
2024-01-02 22:32:13 +0000

    Fixed import

--- dire_wolf_design/dire_wolf_design/settings.py
+++ dire_wolf_design/dire_wolf_design/settings.py
@@ -125,5 +125,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import django_heroku
-django_heroku.settings(locals())
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
