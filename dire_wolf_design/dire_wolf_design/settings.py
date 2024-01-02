9d992cd338b36a03ae9888c2a368980c27b12199
Will Sims <willsims14@gmail.com>
2024-01-02 22:28:33 +0000

    Moved import

--- dire_wolf_design/dire_wolf_design/settings.py
+++ dire_wolf_design/dire_wolf_design/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
-import django_heroku
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,3 +124,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
